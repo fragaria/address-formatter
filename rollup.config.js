c51492a0924792709b5829f9778c80224ff7ab8b
Jirka Chadima <chadima.jiri@gmail.com>
2019-02-21 16:56:16 +0100

    fix: Build pipeline for es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
-
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+
 import pkg from './package.json';
 
 export default [
@@ -19,6 +20,9 @@ export default [
       json(),
       resolve(),
       commonjs(),
+      babel({
+        exclude: ['node_modules/**']
+      }),
       terser(),
     ],
   },
@@ -32,6 +36,9 @@ export default [
       json(),
       resolve(),
       commonjs(),
+      babel({
+        exclude: ['node_modules/**']
+      }),
     ],
   },
 ];
