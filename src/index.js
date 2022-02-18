71a6aee2b73c2ba3369efcc1001d5f5e122e38b1
“Soukaina” <soukaina@cervest.earth>
2022-02-18 13:44:33 +0000

    Make options attributes optionnal + code cleanup

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ const internal = require('./internal.js');
 const countryNames = require('./templates/country-names.json');
 
 module.exports = {
-  format: (input, options = {
-    abbreviate: false,
-    appendCountry: false,
-    cleanupPostcode: true,
-    countryCode: undefined,
-    fallbackCountryCode: undefined,
-    output: 'string'
-  }) => {
+  format: (input, options) => {
+    const {
+      appendCountry= false,
+      countryCode= undefined,
+      fallbackCountryCode= undefined,
+      output= 'string'
+    } = options ||{}
     let realInput = Object.assign({}, input);
     realInput = internal.normalizeComponentKeys(realInput);
-    if (options.countryCode) {
+    if (countryCode) {
       // eslint-disable-next-line camelcase
-      realInput.country_code = options.countryCode;
+      realInput.country_code = countryCode;
     }
-    realInput = internal.determineCountryCode(realInput, options.fallbackCountryCode);
-    if (options.appendCountry && countryNames[realInput.country_code] && !realInput.country) {
+    realInput = internal.determineCountryCode(realInput, fallbackCountryCode);
+    if (appendCountry && countryNames[realInput.country_code] && !realInput.country) {
       realInput.country = countryNames[realInput.country_code];
     }
     realInput = internal.applyAliases(realInput);
     const template = internal.findTemplate(realInput);
     realInput = internal.cleanupInput(realInput, template.replace, options);
     const result = internal.renderTemplate(template, realInput);
-    if (options.output === 'array') {
+    if (output === 'array') {
       return result.split('\n').filter((f) => !!f);
     }
     return result;
