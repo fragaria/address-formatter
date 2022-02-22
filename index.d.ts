b27fb0ced59368a6bd7b4950a243ed1f7768b1bb
“Soukaina” <soukaina@cervest.earth>
2022-02-22 11:21:16 +0000

    fix formatting + updates types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 export function format(input: object, options?: {
-    abbreviate: boolean;
-    appendCountry: boolean;
-    cleanupPostcode: boolean;
-    countryCode: string;
-    fallbackCountryCode: string;
-    output: 'string' | 'array';
+    abbreviate?: boolean;
+    appendCountry?: boolean;
+    cleanupPostcode?: boolean;
+    countryCode?: string;
+    fallbackCountryCode?: string;
+    output?: 'string' | 'array';
 }): string | string[];
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const internal = require('./internal.js');
-const countryNames = require('./templates/country-names.json');
+const internal = require('./internal.js')
+const countryNames = require('./templates/country-names.json')
 
 module.exports = {
   format: (input, options) => {
     const {
-      appendCountry= false,
-      countryCode= undefined,
-      fallbackCountryCode= undefined,
-      output= 'string'
-    } = options ||{}
-    let realInput = Object.assign({}, input);
-    realInput = internal.normalizeComponentKeys(realInput);
+      appendCountry = false,
+      countryCode = undefined,
+      fallbackCountryCode = undefined,
+      output = 'string'
+    } = options || {}
+    let realInput = Object.assign({}, input)
+    realInput = internal.normalizeComponentKeys(realInput)
     if (countryCode) {
       // eslint-disable-next-line camelcase
-      realInput.country_code = countryCode;
+      realInput.country_code = countryCode
     }
-    realInput = internal.determineCountryCode(realInput, fallbackCountryCode);
+    realInput = internal.determineCountryCode(realInput, fallbackCountryCode)
     if (appendCountry && countryNames[realInput.country_code] && !realInput.country) {
-      realInput.country = countryNames[realInput.country_code];
+      realInput.country = countryNames[realInput.country_code]
     }
-    realInput = internal.applyAliases(realInput);
-    const template = internal.findTemplate(realInput);
-    realInput = internal.cleanupInput(realInput, template.replace, options);
-    const result = internal.renderTemplate(template, realInput);
+    realInput = internal.applyAliases(realInput)
+    const template = internal.findTemplate(realInput)
+    realInput = internal.cleanupInput(realInput, template.replace, options)
+    const result = internal.renderTemplate(template, realInput)
     if (output === 'array') {
-      return result.split('\n').filter((f) => !!f);
+      return result.split('\n').filter((f) => !!f)
     }
-    return result;
+    return result
   }
-};
+}
