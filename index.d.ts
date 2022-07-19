88a53ed5adc2cfc09fd71363ad2eb1eac177bb4e
Dmytro Borysov <4720760+dborysov@users.noreply.github.com>
2022-07-19 18:47:30 +0200

    chore: fix typings (#617)

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,84 @@
-export function format(input: object, options?: {
-    abbreviate: boolean;
-    appendCountry: boolean;
-    cleanupPostcode: boolean;
-    countryCode: string;
-    fallbackCountryCode: string;
-    output: 'string' | 'array';
-}): string | string[];
+type PrimaryInputTypes =
+  | 'archipelago'
+  | 'city'
+  | 'continent'
+  | 'country'
+  | 'countryCode'
+  | 'county'
+  | 'hamlet'
+  | 'house'
+  | 'houseNumber'
+  | 'island'
+  | 'municipality'
+  | 'neighbourhood'
+  | 'postalCity'
+  | 'postcode'
+  | 'region'
+  | 'road'
+  | 'state'
+  | 'stateDistrict'
+  | 'village';
+
+type AliasInputTypes =
+  | 'allotments'
+  | 'borough'
+  | 'building'
+  | 'cityBlock'
+  | 'cityDistrict'
+  | 'commercial'
+  | 'countryName'
+  | 'countyCode'
+  | 'croft'
+  | 'department'
+  | 'district'
+  | 'farmland'
+  | 'footway'
+  | 'housenumber'
+  | 'houses'
+  | 'industrial'
+  | 'isolatedDwelling'
+  | 'localAdministrativeArea'
+  | 'locality'
+  | 'partialPostcode'
+  | 'path'
+  | 'pedestrian'
+  | 'place'
+  | 'postcode'
+  | 'province'
+  | 'publicBuilding'
+  | 'quarter'
+  | 'residential'
+  | 'roadReference'
+  | 'roadReferenceIntl'
+  | 'square'
+  | 'stateCode'
+  | 'street'
+  | 'streetName'
+  | 'streetNumber'
+  | 'subcounty'
+  | 'subdistrict'
+  | 'subdivision'
+  | 'suburb'
+  | 'town'
+  | 'township'
+  | 'ward';
+
+type Input = Partial<Record<PrimaryInputTypes | AliasInputTypes, string>>;
+
+interface CommonOptions {
+  abbreviate?: boolean;
+  appendCountry?: boolean;
+  cleanupPostcode?: boolean;
+  countryCode?: string;
+  fallbackCountryCode?: string;
+}
+
+export function format(
+  input: Input,
+  options?: CommonOptions & {output?: 'string'},
+): string;
+
+export function format(
+  input: Input,
+  options: CommonOptions & {output: 'array'},
+): string[];
