df59aeed881aede96536366180be0f6c557b4368
chrisweb <chris963@gmail.com>
2022-10-01 03:41:18 +0200

    remove second index.d.ts fix from first PR

--- index.d.ts
+++ index.d.ts
@@ -1,91 +1,86 @@
-declare namespace addressFormatter {
+type AttentionInputType = 'attention';
 
-    type AttentionInputType = 'attention';
+type PrimaryInputTypes =
+  | 'archipelago'
+  | 'city'
+  | 'continent'
+  | 'country'
+  | 'countryCode'
+  | 'county'
+  | 'hamlet'
+  | 'house'
+  | 'houseNumber'
+  | 'island'
+  | 'municipality'
+  | 'neighbourhood'
+  | 'postalCity'
+  | 'postcode'
+  | 'region'
+  | 'road'
+  | 'state'
+  | 'stateDistrict'
+  | 'village';
 
-    type PrimaryInputTypes =
-        | 'archipelago'
-        | 'city'
-        | 'continent'
-        | 'country'
-        | 'countryCode'
-        | 'county'
-        | 'hamlet'
-        | 'house'
-        | 'houseNumber'
-        | 'island'
-        | 'municipality'
-        | 'neighbourhood'
-        | 'postalCity'
-        | 'postcode'
-        | 'region'
-        | 'road'
-        | 'state'
-        | 'stateDistrict'
-        | 'village';
+type AliasInputTypes =
+  | 'allotments'
+  | 'borough'
+  | 'building'
+  | 'cityBlock'
+  | 'cityDistrict'
+  | 'commercial'
+  | 'countryName'
+  | 'countyCode'
+  | 'croft'
+  | 'department'
+  | 'district'
+  | 'farmland'
+  | 'footway'
+  | 'housenumber'
+  | 'houses'
+  | 'industrial'
+  | 'isolatedDwelling'
+  | 'localAdministrativeArea'
+  | 'locality'
+  | 'partialPostcode'
+  | 'path'
+  | 'pedestrian'
+  | 'place'
+  | 'postcode'
+  | 'province'
+  | 'publicBuilding'
+  | 'quarter'
+  | 'residential'
+  | 'roadReference'
+  | 'roadReferenceIntl'
+  | 'square'
+  | 'stateCode'
+  | 'street'
+  | 'streetName'
+  | 'streetNumber'
+  | 'subcounty'
+  | 'subdistrict'
+  | 'subdivision'
+  | 'suburb'
+  | 'town'
+  | 'township'
+  | 'ward';
 
-    type AliasInputTypes =
-        | 'allotments'
-        | 'borough'
-        | 'building'
-        | 'cityBlock'
-        | 'cityDistrict'
-        | 'commercial'
-        | 'countryName'
-        | 'countyCode'
-        | 'croft'
-        | 'department'
-        | 'district'
-        | 'farmland'
-        | 'footway'
-        | 'housenumber'
-        | 'houses'
-        | 'industrial'
-        | 'isolatedDwelling'
-        | 'localAdministrativeArea'
-        | 'locality'
-        | 'partialPostcode'
-        | 'path'
-        | 'pedestrian'
-        | 'place'
-        | 'postcode'
-        | 'province'
-        | 'publicBuilding'
-        | 'quarter'
-        | 'residential'
-        | 'roadReference'
-        | 'roadReferenceIntl'
-        | 'square'
-        | 'stateCode'
-        | 'street'
-        | 'streetName'
-        | 'streetNumber'
-        | 'subcounty'
-        | 'subdistrict'
-        | 'subdivision'
-        | 'suburb'
-        | 'town'
-        | 'township'
-        | 'ward';
+type Input = Partial<Record<AttentionInputType | PrimaryInputTypes | AliasInputTypes, string>>;
 
-    type Input = Partial<Record<AttentionInputType | PrimaryInputTypes | AliasInputTypes, string>>;
-
-    interface CommonOptions {
-        abbreviate?: boolean;
-        appendCountry?: boolean;
-        cleanupPostcode?: boolean;
-        countryCode?: string;
-        fallbackCountryCode?: string;
-    }
-
-    export function format(
-        input: Input,
-        options?: CommonOptions & { output?: 'string' },
-    ): string;
-
-    export function format(
-        input: Input,
-        options: CommonOptions & { output: 'array' },
-    ): string[];
+interface CommonOptions {
+  abbreviate?: boolean;
+  appendCountry?: boolean;
+  cleanupPostcode?: boolean;
+  countryCode?: string;
+  fallbackCountryCode?: string;
 }
 
-export = addressFormatter;
+export function format(
+  input: Input,
+  options?: CommonOptions & {output?: 'string'},
+): string;
+
+export function format(
+  input: Input,
+  options: CommonOptions & {output: 'array'},
+): string[];
