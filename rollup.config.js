53c064b97829018d93cec105011720ad4d7e2308
Jiří Chadima <chadima.jiri@gmail.com>
2024-06-23 09:26:13 +0200

    Update to node 20 (#884)
    * Requires node 20
    
    * Updates definitions from address-formatting
    
    * Uses node 20 on github
    
    * More deps updates

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 import pkg from './package.json';
 
